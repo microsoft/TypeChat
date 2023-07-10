915aa5eedcb1c37ac8b034694bdf38319fdd328a
Steve Lucco <steveluc@users.noreply.github.com>
2023-07-10 16:29:40 -0700

    Async tcp (#12)
    * Add TypeChatProgramValidator object and 'music' example that shows use
    
    * Introduce TypeChatFunctionValidator<T>
    
    * Update examples/music
    
    * Add new examples/code sample
    
    * Minor fixes
    
    * Restrict arrow functions but permit assignment operators
    
    * Add TypeChatFunctionTranslator<T>
    
    * Update examples
    
    * Change "\r" to "\n"
    
    * No 'any' types in default library
    
    * Rename TypeChat<T> to TypeChatJsonTranslator<T>
    
    * Update examples
    
    * connect music app to Spotify and re-introduce JSON version
    
    ---------
    
    Co-authored-by: Anders Hejlsberg <andersh@microsoft.com>

---
+++ examples/music/src/chatifyJSONSchema.ts
@@ -0,0 +1,139 @@
+export interface ActionWithInput {
+    // use results from action indexed by inputFromAction; default use results from previous action
+    inputFromAction?: number;
+}
+
+export type FavoritesTerm =
+    | 'short_term' // last 4 weeks
+    | 'medium_term' // last 6 months
+    | 'long_term'; // several years
+
+// output: Track List
+// get the tracks played most recently
+export interface GetRecentlyPlayedAction {
+    type: 'getRecentlyPlayed';
+    // if favoritesTerm is specified, get the user's top tracks over the specified time range
+    favoritesTerm?: FavoritesTerm;
+    // get count tracks; default is 50
+    count?: number;
+}
+
+export interface PauseAction {
+    type: 'pause';
+}
+
+export interface PlayAction {
+    type: 'play';
+}
+
+// Delete a named playlist
+export interface DeletePlaylistAction {
+    type: 'deletePlaylist';
+    // the name of the playlist to delete
+    playlistName: string;
+}
+export interface VolumeAction {
+    type: 'setVolume';
+    // 0 is silent 100 is the loudest
+    newVolumeLevel?: number;
+    // volumeChangeAmount can be positive or negative; example: -5 makes the volume 5% more quiet
+    volumeChangeAmount?: number;
+}
+
+// output: Track List
+export interface SearchTracksAction {
+    type: 'searchTracks';
+    // a Spotify search expression such as 'Rock Lobster' or 'te kanawa queen of night'
+    // the structure of the search expression is keywords separated by spaces; all keywords must match
+    query: string;
+}
+
+// input: Track List
+// play some or all items from the input list
+export interface PlayInputAction extends ActionWithInput {
+    type: 'playInput';
+    // number of tracks to play; default 1
+    count?: number;
+    // index of first track to play; default 0
+    offset?: number;
+    // play all of the tracks; default false and ignored if count or offset present
+    all?: boolean;
+}
+
+// input: Track List
+// print out some or all items from the input list
+export interface ListInputAction extends ActionWithInput {
+    type: 'listInput';
+    // number of tracks to print out; default input.length
+    count?: number;
+    // index of first track to list; default 0
+    offset?: number;
+    // list all of the tracks; default true but overridden by presence of count or offset
+    all?: boolean;
+}
+// input: Track List; output: Track List
+// apply a filter to match tracks; result is the tracks that match the filter
+export interface FilterTracksAction extends ActionWithInput {
+    type: 'filterTracks';
+    // a filter string, which has the following structure (written as a grammar)
+    // filter -> (constraint combiner?)+
+    // constraint -> artist:string | genre:string | year:year-range | description:string
+    // combiner -> 'AND' | 'OR' (AND is the default and may be omitted)
+    // the description constraint value describes any constraint not expressable as a combination of genre, artist and year constraints
+    filter: string;
+    // keep the tracks that do not match, instead of the tracks that match; default false
+    negate?: boolean;
+}
+
+// input: Track List; output: Track List
+// sort the tracks; default is sort by track name ascending
+export interface SortTracksAction extends ActionWithInput {
+    type: 'sortTracks';
+    // default: false
+    descending?: boolean;
+}
+
+// input: Track List
+// create a Spotify playlist from a list of tracks
+export interface CreatePlaylistAction extends ActionWithInput {
+    type: 'createPlaylist';
+    name: string;
+}
+
+export interface ListPlaylistsAction {
+    type: 'listPlaylists';
+}
+
+// if the user types text that is not understood, this action is used
+export interface UnknownAction {
+    type: 'unknown';
+    // text typed by the user that the system did not understand
+    text: string;
+}
+
+// if the user asks a non-music question, it is captured with this action; non-music, non-questions use UnknownAction
+export interface NonMusicQuestionAction {
+    type: 'nonMusicQuestion';
+    // text of the question
+    text: string;
+}
+
+export type SpotifyAction =
+    | PlayAction
+    | PlayInputAction
+    | ListInputAction
+    | SearchTracksAction
+    | PauseAction
+    | VolumeAction
+    | GetRecentlyPlayedAction
+    | FilterTracksAction
+    | SortTracksAction
+    | CreatePlaylistAction
+    | DeletePlaylistAction
+    | ListPlaylistsAction
+    | NonMusicQuestionAction
+    | UnknownAction;
+
+export type SpotifyActions = {
+    actions: SpotifyAction[];
+};
