b096921ee7e401e2e99cfaa16a36deb14ee6b228
Anders Hejlsberg <andersh@microsoft.com>
2023-07-13 10:59:33 -1000

    Improve prompt to fix error observed with gpt-4

--- examples/programs/src/chatifyActionsSchema.ts
+++ examples/programs/src/chatifyActionsSchema.ts
@@ -94,12 +94,13 @@ export type Program = {
 }
 
 // An expression is a JSON value, a function call, or a reference to the result of a preceding expression.
+// Function calls and result references may occur at any level of nesting.
 export type Expression = JSONValue | FunctionCall | ResultReference;
 
 // Represents a JSON value.
 export type JSONValue = string | number | boolean | null | { [x: string]: Expression } | Expression[];
 
-// Represents a call to one of the API functions. Function calls may be nested.
+// Represents a call to one of the API functions. Function calls may be nested in JSON objects.
 export type FunctionCall = {
     // Name of the API function called by this expression
     "@func": keyof API;
@@ -107,7 +108,7 @@ export type FunctionCall = {
     "@args": Expression[];
 };
 
-// Represents a reference to the result of a preceding expression.
+// Represents a reference to the result of a preceding expression. Result references may be nested in JSON objects.
 export type ResultReference = {
     // Index of the previous expression in the expressions array
     "@ref": number;
