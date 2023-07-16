6e34cfa57af34b32b2d2d30ba2784699e3e054a2
Anders Hejlsberg <andersh@microsoft.com>
2023-07-16 15:38:51 -0700

    Update music example

--- examples/music/src/chatifyActionsSchema.ts
+++ examples/music/src/chatifyActionsSchema.ts
@@ -56,7 +56,7 @@ export type SortTracksArgs = {
     descending?: boolean;
 }
 
-type API = {
+export type API = {
     // Get the tracks played most recently
     getRecentlyPlayed(options?: GetRecentlyPlayedOptions): TrackList;
     // Pause playing
@@ -89,29 +89,3 @@ type API = {
     // Call this function with the final result of the user request, if any
     finalResult(result: any): void;
 }
-
-// A program consists of a sequence of expressions that are executed in order.
-export type Program = {
-    expressions: Expression[];
-}
-
-// An expression is a JSON value, a function call, or a reference to the result of a preceding expression.
-// Function calls and result references may occur at any level of nesting.
-export type Expression = JSONValue | FunctionCall | ResultReference;
-
-// Represents a JSON value.
-export type JSONValue = string | number | boolean | null | { [x: string]: Expression } | Expression[];
-
-// Represents a call to one of the API functions. Function calls may be nested in JSON objects.
-export type FunctionCall = {
-    // Name of the API function called by this expression
-    "@func": keyof API;
-    // Arguments for the function
-    "@args": Expression[];
-};
-
-// Represents a reference to the result of a preceding expression. Result references may be nested in JSON objects.
-export type ResultReference = {
-    // Index of the previous expression in the expressions array
-    "@ref": number;
-};
