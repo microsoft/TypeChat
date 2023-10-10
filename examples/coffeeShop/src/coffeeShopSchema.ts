a76e72c28155f5c0f3548d2762a8e92de555b66b
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2023-10-09 22:31:11 -0700

    Quote style.

--- examples/coffeeShop/src/coffeeShopSchema.ts
+++ examples/coffeeShop/src/coffeeShopSchema.ts
@@ -6,12 +6,12 @@ export interface Cart {
 
 // Use this type for order items that match nothing else
 export interface UnknownText {
-    type: 'unknown',
+    type: "unknown",
     text: string; // The text that wasn't understood
 }
 
 export interface LineItem {
-    type: 'lineitem',
+    type: "lineitem",
     product: Product;
     quantity: number;
 }
@@ -19,91 +19,91 @@ export interface LineItem {
 export type Product = BakeryProducts | LatteDrinks | EspressoDrinks | CoffeeDrinks;
 
 export interface BakeryProducts {
-    type: 'BakeryProducts';
-    name: 'apple bran muffin' | 'blueberry muffin' | 'lemon poppyseed muffin' | 'bagel';
+    type: "BakeryProducts";
+    name: "apple bran muffin" | "blueberry muffin" | "lemon poppyseed muffin" | "bagel";
     options: (BakeryOptions | BakeryPreparations)[];
 }
 
 export interface BakeryOptions {
-    type: 'BakeryOptions';
-    name: 'butter' | 'strawberry jam' | 'cream cheese';
+    type: "BakeryOptions";
+    name: "butter" | "strawberry jam" | "cream cheese";
     optionQuantity?: OptionQuantity;
 }
 
 export interface BakeryPreparations {
-    type: 'BakeryPreparations';
-    name: 'warmed' | 'cut in half';
+    type: "BakeryPreparations";
+    name: "warmed" | "cut in half";
 }
 
 export interface LatteDrinks {
-    type: 'LatteDrinks';
-    name: 'cappuccino' | 'flat white' | 'latte' | 'latte macchiato' | 'mocha' | 'chai latte';
+    type: "LatteDrinks";
+    name: "cappuccino" | "flat white" | "latte" | "latte macchiato" | "mocha" | "chai latte";
     temperature?: CoffeeTemperature;
-    size?: CoffeeSize;  // The default is 'grande'
+    size?: CoffeeSize;  // The default is "grande"
     options?: (Milks | Sweeteners | Syrups | Toppings | Caffeines | LattePreparations)[];
 }
 
 export interface EspressoDrinks {
-    type: 'EspressoDrinks';
-    name: 'espresso' | 'lungo' | 'ristretto' | 'macchiato';
+    type: "EspressoDrinks";
+    name: "espresso" | "lungo" | "ristretto" | "macchiato";
     temperature?: CoffeeTemperature;
-    size?: EspressoSize;  // The default is 'doppio'
+    size?: EspressoSize;  // The default is "doppio"
     options?: (Creamers | Sweeteners | Syrups | Toppings | Caffeines | LattePreparations)[];
 }
 
 export interface CoffeeDrinks {
-    type: 'CoffeeDrinks';
-    name: 'americano' | 'coffee';
+    type: "CoffeeDrinks";
+    name: "americano" | "coffee";
     temperature?: CoffeeTemperature;
-    size?: CoffeeSize;  // The default is 'grande'
+    size?: CoffeeSize;  // The default is "grande"
     options?: (Creamers | Sweeteners | Syrups | Toppings | Caffeines | LattePreparations)[];
 }
 
 export interface Syrups {
-    type: 'Syrups';
-    name: 'almond syrup' | 'buttered rum syrup' | 'caramel syrup' | 'cinnamon syrup' | 'hazelnut syrup' |
-        'orange syrup' | 'peppermint syrup' | 'raspberry syrup' | 'toffee syrup' | 'vanilla syrup';
+    type: "Syrups";
+    name: "almond syrup" | "buttered rum syrup" | "caramel syrup" | "cinnamon syrup" | "hazelnut syrup" |
+        "orange syrup" | "peppermint syrup" | "raspberry syrup" | "toffee syrup" | "vanilla syrup";
     optionQuantity?: OptionQuantity;
 }
 
 export interface Caffeines {
-    type: 'Caffeines';
-    name: 'regular' | 'two thirds caf' | 'half caf' | 'one third caf' | 'decaf';
+    type: "Caffeines";
+    name: "regular" | "two thirds caf" | "half caf" | "one third caf" | "decaf";
 }
 
 export interface Milks {
-    type: 'Milks';
-    name: 'whole milk' | 'two percent milk' | 'nonfat milk' | 'coconut milk' | 'soy milk' | 'almond milk' | 'oat milk';
+    type: "Milks";
+    name: "whole milk" | "two percent milk" | "nonfat milk" | "coconut milk" | "soy milk" | "almond milk" | "oat milk";
 }
 
 export interface Creamers {
-    type: 'Creamers';
-    name: 'whole milk creamer' | 'two percent milk creamer' | 'one percent milk creamer' | 'nonfat milk creamer' |
-        'coconut milk creamer' | 'soy milk creamer' | 'almond milk creamer' | 'oat milk creamer' | 'half and half' |
-        'heavy cream';
+    type: "Creamers";
+    name: "whole milk creamer" | "two percent milk creamer" | "one percent milk creamer" | "nonfat milk creamer" |
+        "coconut milk creamer" | "soy milk creamer" | "almond milk creamer" | "oat milk creamer" | "half and half" |
+        "heavy cream";
 }
 
 export interface Toppings {
-    type: 'Toppings';
-    name: 'cinnamon' | 'foam' | 'ice' | 'nutmeg' | 'whipped cream' | 'water';
+    type: "Toppings";
+    name: "cinnamon" | "foam" | "ice" | "nutmeg" | "whipped cream" | "water";
     optionQuantity?: OptionQuantity;
 }
 
 export interface LattePreparations {
-    type: 'LattePreparations';
-    name: 'for here cup' | 'lid' | 'with room' | 'to go' | 'dry' | 'wet';
+    type: "LattePreparations";
+    name: "for here cup" | "lid" | "with room" | "to go" | "dry" | "wet";
 }
 
 export interface Sweeteners {
-    type: 'Sweeteners';
-    name: 'equal' | 'honey' | 'splenda' | 'sugar' | 'sugar in the raw' | 'sweet n low' | 'espresso shot';
+    type: "Sweeteners";
+    name: "equal" | "honey" | "splenda" | "sugar" | "sugar in the raw" | "sweet n low" | "espresso shot";
     optionQuantity?: OptionQuantity;
 }
 
-export type CoffeeTemperature = 'hot' | 'extra hot' | 'warm' | 'iced';
+export type CoffeeTemperature = "hot" | "extra hot" | "warm" | "iced";
 
-export type CoffeeSize = 'short' | 'tall' | 'grande' | 'venti';
+export type CoffeeSize = "short" | "tall" | "grande" | "venti";
 
-export type EspressoSize = 'solo' | 'doppio' | 'triple' | 'quad';
+export type EspressoSize = "solo" | "doppio" | "triple" | "quad";
 
-export type OptionQuantity = 'no' | 'light' | 'regular' | 'extra' | number;
+export type OptionQuantity = "no" | "light" | "regular" | "extra" | number;
