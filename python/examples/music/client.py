0f342e3b663e1e415ee2f32ea058bd625a9ad021
Hillary Mutisya <hillarym@microsoft.com>
2024-01-03 03:28:18 -0800

    Add notebook for music example

--- python/examples/music/client.py
+++ python/examples/music/client.py
@@ -1,3 +1,9 @@
+import os
+import sys
+current_path = os.path.abspath(os.path.dirname(__file__))
+if current_path not in sys.path:
+    sys.path.append(current_path)
+
 import math
 from typing import Any, Optional
 
