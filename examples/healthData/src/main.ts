093f15fe9fda3b3dbf95e5096535225bc972eccf
Piali Choudhury <pialic@microsoft.com>
2024-01-05 13:45:26 -0800

    ongoing work on the example

--- examples/healthData/src/main.ts
+++ examples/healthData/src/main.ts
@@ -8,18 +8,44 @@ import { HealthDataResponse } from "./healthDataSchema";
 // TODO: use local .env file.
 dotenv.config({ path: path.join(__dirname, "../../../.env") });
 
+const health_instructions:string = `
+                            Help me enter my health data step by step.
+                            Ask specific questions to gather required and optional fields
+                            I have not already providedStop asking if I don't know the answer
+                            Automatically fix my spelling mistakes
+                            My health data may be complex: always record and return ALL of it.
+                            Always return a response:
+                            - If you don't understand what I say, ask a question.
+                            - At least respond with an OK message.
+                            `;
+
 const model = createLanguageModel(process.env);
 const schema = fs.readFileSync(path.join(__dirname, "healthDataSchema.ts"), "utf8");
-const translator = createHealthDataTranslator<HealthDataResponse>(model, schema, "HealthDataResponse", "");
+const translator = createHealthDataTranslator<HealthDataResponse>(model, schema, "HealthDataResponse",
+                        health_instructions);
 
 // Process requests interactively or from the input file specified on the command line
 processRequests("💉💊🤧> ", process.argv[2], async (request) => {
     const response = await translator.translate(request);
     if (!response.success) {
-        console.log(response.message);
-        return;
+        console.log("Translation Failed ❌");
+        console.log(`Context: ${response.message}`);
     }
-    const healthdata = response.data;
-    console.log(JSON.stringify(healthdata, undefined, 2));
-    console.log("Success!");
+    else {
+        const healthdata = response.data;
+        console.log("Translation Succeeded! ✅\n");
+        console.log("JSON View");
+        console.log(JSON.stringify(healthdata, undefined, 2));
+
+        const message = healthdata["message"];
+        const not_translated = healthdata["notTranslated"];
+
+        if (message) {
+            console.log(`\n📝: ${message}`);
+        }
+            
+        if (not_translated) {
+            console.log(`\n🤔: I did not understand\n ${not_translated}`)
+        }
+    }
 });
--- examples/healthData/src/translator.ts
+++ examples/healthData/src/translator.ts
@@ -1,4 +1,4 @@
-import {Result, TypeChatLanguageModel, createJsonTranslator, TypeChatJsonTranslator, PromptSection} from "typechat";
+import {Result, TypeChatLanguageModel, createJsonTranslator, TypeChatJsonTranslator} from "typechat";
 
 type ChatMessage = {
     source: "system" | "user" | "assistant";
@@ -10,7 +10,7 @@ export interface TranslatorWithHistory<T extends object> {
     _max_prompt_length: number;
     _additional_agent_instructions: string;
     translator: TypeChatJsonTranslator<T>;
-    translate(request: string, promptPreamble?: string | PromptSection[]): Promise<Result<T>>;
+    translate(request: string): Promise<Result<T>>;
 }
 
 export function createHealthDataTranslator<T extends object>(model: TypeChatLanguageModel, schema: string, type_name: string, additional_agent_instructions: string): TranslatorWithHistory<T> {
@@ -31,8 +31,8 @@ export function createHealthDataTranslator<T extends object>(model: TypeChatLang
 
     return customtranslator;
 
-    async function translate(request: string, promptPreamble?: string | PromptSection[]): Promise<Result<T>> {
-        const response = await translator.translate(request, promptPreamble);
+    async function translate(request: string): Promise<Result<T>> {
+        const response = await translator.translate(request);
         if (response.success) {
             _chat_history.push({ source: 'assistant', body: JSON.stringify(response.data) });
         }
@@ -40,36 +40,36 @@ export function createHealthDataTranslator<T extends object>(model: TypeChatLang
     }
 
     function _create_request_prompt(intent: string): string {
-    // TODO: drop history entries if we exceed the max_prompt_length
-    const history_str = JSON.stringify(_chat_history, null, 2);
+        // TODO: drop history entries if we exceed the max_prompt_length
+        const history_str = JSON.stringify(_chat_history, undefined, 2);
+        
+        const now = new Date();
 
-    const now = new Date();
+        const prompt = `
+        user: You are a service that translates user requests into JSON objects of type "${type_name}" according to the following TypeScript definitions:
+        '''
+        ${schema}
+        '''
 
-    const prompt = `
-      user: You are a service that translates user requests into JSON objects of type "${type_name}" according to the following TypeScript definitions:
-      '''
-      ${schema}
-      '''
+        user:
+        Use precise date and times RELATIVE TO CURRENT DATE: ${now.toLocaleDateString()} CURRENT TIME: ${now.toTimeString().split(' ')[0]}
+        Also turn ranges like next week and next month into precise dates
+        
+        user:
+        ${_additional_agent_instructions}
+        
+        system:
+        IMPORTANT CONTEXT for the user request:
+        ${history_str}
 
-      user:
-      Use precise date and times RELATIVE TO CURRENT DATE: ${now.toLocaleDateString()} CURRENT TIME: ${now.toTimeString().split(' ')[0]}
-      Also turn ranges like next week and next month into precise dates
-      
-      user:
-      ${_additional_agent_instructions}
-      
-      system:
-      IMPORTANT CONTEXT for the user request:
-      ${history_str}
-
-      user:
-      The following is a user request:
-      '''
-      ${intent}
-      '''
-      The following is the user request translated into a JSON object with 2 spaces of indentation and no properties with the value undefined:
-    `;
-
-    return prompt;
-  }
+        user:
+        The following is a user request:
+        '''
+        ${intent}
+        '''
+        The following is the user request translated into a JSON object with 2 spaces of indentation and no properties with the value undefined:
+        """
+        `;
+        return prompt;
+    }
 }
