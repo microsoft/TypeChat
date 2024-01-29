cf9fe253cf1dced01f6070a64e21efac2b118b99
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-01-29 13:01:48 -0700

    Prettier formatting

--- src/model.ts
+++ src/model.ts
@@ -1,4 +1,4 @@
-import { Result, success, error } from "./result";
+import { Result, success, error } from './result';
 
 /**
  * Represents a section of an LLM prompt with an associated role. TypeChat uses the "user" role for
@@ -6,14 +6,14 @@ import { Result, success, error } from "./result";
  * the prompt in repair attempts). TypeChat currently doesn't use the "system" role.
  */
 export interface PromptSection {
-    /**
-     * Specifies the role of this section.
-     */
-    role: "system" | "user" | "assistant";
-    /**
-     * Specifies the content of this section.
-     */
-    content: string;
+	/**
+	 * Specifies the role of this section.
+	 */
+	role: 'system' | 'user' | 'assistant';
+	/**
+	 * Specifies the content of this section.
+	 */
+	content: string;
 }
 
 /**
@@ -23,20 +23,20 @@ export interface PromptSection {
  * and `createAzureOpenAILanguageModel` functions create instances of this interface.
  */
 export interface TypeChatLanguageModel {
-    /**
-     * Optional property that specifies the maximum number of retry attempts (the default is 3).
-     */
-    retryMaxAttempts?: number;
-    /**
-     * Optional property that specifies the delay before retrying in milliseconds (the default is 1000ms).
-     */
-    retryPauseMs?: number;
-    /**
-     * Obtains a completion from the language model for the given prompt.
-     * @param prompt A prompt string or an array of prompt sections. If a string is specified,
-     *   it is converted into a single "user" role prompt section.
-     */
-    complete(prompt: string | PromptSection[]): Promise<Result<string>>;
+	/**
+	 * Optional property that specifies the maximum number of retry attempts (the default is 3).
+	 */
+	retryMaxAttempts?: number;
+	/**
+	 * Optional property that specifies the delay before retrying in milliseconds (the default is 1000ms).
+	 */
+	retryPauseMs?: number;
+	/**
+	 * Obtains a completion from the language model for the given prompt.
+	 * @param prompt A prompt string or an array of prompt sections. If a string is specified,
+	 *   it is converted into a single "user" role prompt section.
+	 */
+	complete(prompt: string | PromptSection[]): Promise<Result<string>>;
 }
 
 /**
@@ -55,19 +55,19 @@ export interface TypeChatLanguageModel {
  * @returns An instance of `TypeChatLanguageModel`.
  */
 export function createLanguageModel(env: Record<string, string | undefined>): TypeChatLanguageModel {
-    if (env.OPENAI_API_KEY) {
-        const apiKey = env.OPENAI_API_KEY ?? missingEnvironmentVariable("OPENAI_API_KEY");
-        const model = env.OPENAI_MODEL ?? missingEnvironmentVariable("OPENAI_MODEL");
-        const endPoint = env.OPENAI_ENDPOINT ?? "https://api.openai.com/v1/chat/completions";
-        const org = env.OPENAI_ORGANIZATION ?? "";
-        return createOpenAILanguageModel(apiKey, model, endPoint, org);
-    }
-    if (env.AZURE_OPENAI_API_KEY) {
-        const apiKey = env.AZURE_OPENAI_API_KEY ?? missingEnvironmentVariable("AZURE_OPENAI_API_KEY");
-        const endPoint = env.AZURE_OPENAI_ENDPOINT ?? missingEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
-        return createAzureOpenAILanguageModel(apiKey, endPoint);
-    }
-    missingEnvironmentVariable("OPENAI_API_KEY or AZURE_OPENAI_API_KEY");
+	if (env.OPENAI_API_KEY) {
+		const apiKey = env.OPENAI_API_KEY ?? missingEnvironmentVariable('OPENAI_API_KEY');
+		const model = env.OPENAI_MODEL ?? missingEnvironmentVariable('OPENAI_MODEL');
+		const endPoint = env.OPENAI_ENDPOINT ?? 'https://api.openai.com/v1/chat/completions';
+		const org = env.OPENAI_ORGANIZATION ?? '';
+		return createOpenAILanguageModel(apiKey, model, endPoint, org);
+	}
+	if (env.AZURE_OPENAI_API_KEY) {
+		const apiKey = env.AZURE_OPENAI_API_KEY ?? missingEnvironmentVariable('AZURE_OPENAI_API_KEY');
+		const endPoint = env.AZURE_OPENAI_ENDPOINT ?? missingEnvironmentVariable('AZURE_OPENAI_ENDPOINT');
+		return createAzureOpenAILanguageModel(apiKey, endPoint);
+	}
+	missingEnvironmentVariable('OPENAI_API_KEY or AZURE_OPENAI_API_KEY');
 }
 
 /**
@@ -78,12 +78,12 @@ export function createLanguageModel(env: Record<string, string | undefined>): Ty
  * @param org The OpenAI organization id.
  * @returns An instance of `TypeChatLanguageModel`.
  */
-export function createOpenAILanguageModel(apiKey: string, model: string, endPoint = "https://api.openai.com/v1/chat/completions", org = ""): TypeChatLanguageModel {
-    const headers = {
-        "Authorization": `Bearer ${apiKey}`,
-        "OpenAI-Organization": org
-    };
-    return createFetchLanguageModel(endPoint, headers, { model });
+export function createOpenAILanguageModel(apiKey: string, model: string, endPoint = 'https://api.openai.com/v1/chat/completions', org = ''): TypeChatLanguageModel {
+	const headers = {
+		Authorization: `Bearer ${apiKey}`,
+		'OpenAI-Organization': org,
+	};
+	return createFetchLanguageModel(endPoint, headers, { model });
 }
 
 /**
@@ -95,82 +95,82 @@ export function createOpenAILanguageModel(apiKey: string, model: string, endPoin
  * @returns An instance of `TypeChatLanguageModel`.
  */
 export function createAzureOpenAILanguageModel(apiKey: string, endPoint: string): TypeChatLanguageModel {
-    const headers = {
-        // Needed when using managed identity
-        "Authorization": `Bearer ${apiKey}`,
-        // Needed when using regular API key
-        "api-key": apiKey
-    };
-    return createFetchLanguageModel(endPoint, headers, {});
+	const headers = {
+		// Needed when using managed identity
+		Authorization: `Bearer ${apiKey}`,
+		// Needed when using regular API key
+		'api-key': apiKey,
+	};
+	return createFetchLanguageModel(endPoint, headers, {});
 }
 
 /**
  * Common OpenAI REST API endpoint encapsulation using the fetch API.
  */
 function createFetchLanguageModel(url: string, headers: object, defaultParams: object) {
-    const model: TypeChatLanguageModel = {
-        complete
-    };
-    return model;
+	const model: TypeChatLanguageModel = {
+		complete,
+	};
+	return model;
 
-    async function complete(prompt: string | PromptSection[]) {
-        let retryCount = 0;
-        const retryMaxAttempts = model.retryMaxAttempts ?? 3;
-        const retryPauseMs = model.retryPauseMs ?? 1000;
-        const messages = typeof prompt === "string" ? [{ role: "user", content: prompt }] : prompt;
-        while (true) {
-            const options = {
-                method: "POST",
-                body: JSON.stringify({
-                    ...defaultParams,
-                    messages,
-                    temperature: 0,
-                    n: 1
-                }),
-                headers: {
-                    "content-type": "application/json",
-                    ...headers
-                }
-            }
-            const response = await fetch(url, options);
-            if (response.ok) {
-                const json = await response.json() as { choices: { message: PromptSection }[] };
-                return success(json.choices[0].message.content ?? "");
-            }
-            if (!isTransientHttpError(response.status) || retryCount >= retryMaxAttempts) {
-                return error(`REST API error ${response.status}: ${response.statusText}`);
-            }
-            await sleep(retryPauseMs);
-            retryCount++;
-        }
-    }
+	async function complete(prompt: string | PromptSection[]) {
+		let retryCount = 0;
+		const retryMaxAttempts = model.retryMaxAttempts ?? 3;
+		const retryPauseMs = model.retryPauseMs ?? 1000;
+		const messages = typeof prompt === 'string' ? [{ role: 'user', content: prompt }] : prompt;
+		while (true) {
+			const options = {
+				method: 'POST',
+				body: JSON.stringify({
+					...defaultParams,
+					messages,
+					temperature: 0,
+					n: 1,
+				}),
+				headers: {
+					'content-type': 'application/json',
+					...headers,
+				},
+			};
+			const response = await fetch(url, options);
+			if (response.ok) {
+				const json = (await response.json()) as { choices: { message: PromptSection }[] };
+				return success(json.choices[0].message.content ?? '');
+			}
+			if (!isTransientHttpError(response.status) || retryCount >= retryMaxAttempts) {
+				return error(`REST API error ${response.status}: ${response.statusText}`);
+			}
+			await sleep(retryPauseMs);
+			retryCount++;
+		}
+	}
 }
 
 /**
  * Returns true of the given HTTP status code represents a transient error.
  */
 function isTransientHttpError(code: number): boolean {
-    switch (code) {
-        case 429: // TooManyRequests
-        case 500: // InternalServerError
-        case 502: // BadGateway
-        case 503: // ServiceUnavailable
-        case 504: // GatewayTimeout
-            return true;
-    }
-    return false;
+	switch (code) {
+		case 429: // TooManyRequests
+		case 500: // InternalServerError
+		case 502: // BadGateway
+		case 503: // ServiceUnavailable
+		case 504: // GatewayTimeout
+			return true;
+	}
+	return false;
 }
 
 /**
  * Sleeps for the given number of milliseconds.
  */
 function sleep(ms: number): Promise<void> {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 /**
  * Throws an exception for a missing environment variable.
  */
 function missingEnvironmentVariable(name: string): never {
-    throw new Error(`Missing environment variable: ${name}`);
+	throw new Error(`Missing environment variable: ${name}`);
 }
