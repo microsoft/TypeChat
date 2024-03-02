de02a8da1b98b7bda63e8a4ffe5050206d264319
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2024-03-01 17:14:03 -0800

    Fix `typescript/healthData` and search upward for `.env` files in TS examples. (#205)
    * Fix typechat version for `healthData` example.
    
    * Add 'find-config' to search upward for `.env` files, organize imports.

--- typescript/examples/multiSchema/src/main.ts
+++ typescript/examples/multiSchema/src/main.ts
@@ -1,12 +1,15 @@
+import assert from "assert";
+import dotenv from "dotenv";
+import findConfig from "find-config";
 import fs from "fs";
 import path from "path";
-import dotenv from "dotenv";
+import { createLanguageModel, processRequests } from "typechat";
 import { createJsonMathAgent, createJsonPrintAgent } from "./agent";
-import {createLanguageModel, processRequests } from "typechat";
 import { createAgentRouter } from "./router";
 
-// TODO: use local .env file.
-dotenv.config({ path: path.join(__dirname, "../../../.env") });
+const dotEnvPath = findConfig(".env");
+assert(dotEnvPath, ".env file not found!");
+dotenv.config({ path: dotEnvPath });
 
 const model = createLanguageModel(process.env);
 const taskClassificationSchema = fs.readFileSync(path.join(__dirname, "classificationSchema.ts"), "utf8");
