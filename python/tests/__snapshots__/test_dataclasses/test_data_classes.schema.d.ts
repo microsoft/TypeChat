738920bf156aa85bba6493e138141983ff0b0988
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2024-03-12 08:51:06 +0000

    Update snapshots.

--- python/tests/__snapshots__/test_dataclasses/test_data_classes.schema.d.ts
+++ python/tests/__snapshots__/test_dataclasses/test_data_classes.schema.d.ts
@@ -1,15 +1,15 @@
 // Entry point is: 'Response'
 
 interface Response {
-    "attr_1": string;
+    attr_1: string;
     // Hello!
-    "attr_2": number;
-    "attr_3": string | null;
-    "attr_4"?: string;
-    "attr_5"?: string | null;
-    "attr_6"?: string[];
-    "attr_7"?: Options;
-    "_underscore_attr_1"?: number;
+    attr_2: number;
+    attr_3: string | null;
+    attr_4?: string;
+    attr_5?: string | null;
+    attr_6?: string[];
+    attr_7?: Options;
+    _underscore_attr_1?: number;
 }
 
 // TODO: someone add something here.
--- python/tests/__snapshots__/test_generic_alias_2/test_generic_alias1.schema.d.ts
+++ python/tests/__snapshots__/test_generic_alias_2/test_generic_alias1.schema.d.ts
@@ -8,10 +8,10 @@ type FirstOrSecond<T> = First<T> | Second<T>
 
 interface Second<T> {
     kind: "second";
-    "second_attr": T;
+    second_attr: T;
 }
 
 interface First<T> {
     kind: "first";
-    "first_attr": T;
+    first_attr: T;
 }
